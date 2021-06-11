f55a13d389932653b04d45e14a4f96a23467c5dc
Shukrulloh <soginboevshukrullo@gmail.com>
2021-06-11 11:24:28 +0500

    Oybek Portfolio

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+// header scroll bolganda black bolishi
+window.addEventListener("scroll", function() {
+    var navScroll = document.getElementById('nav');
+    navScroll.classList.toggle("fixed", window.scrollY > 5);
+});
+
+function navBack(){
+    var nav = document.getElementById('nav').classList.toggle("change");
+}
